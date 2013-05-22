1f1c231a6527091cba57c834a9073bc6b064dcd1
HuangKangJing <huangkangjing@gmail.com>
2013-05-22 20:45:12 +0800

    Updated DisplayWidget

--- frontend/Display.py
+++ frontend/Display.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python2
 import PyQt4.QtOpenGL as qgl
+import PyQt4.QtCore as c
 import OpenGL.GL as gl
-import OpenGL.GLUT as glut
+# import OpenGL.GLUT as glut
+import galaxy
 
 class DisplayWidget(qgl.QGLWidget):
-    def __init__(self, galaxy, scale, timeout = 100, parent = None):
+    def __init__(self,
+                 galaxy_obj,
+                 scale,
+                 timeout = 100,
+                 parent = None):
         qgl.QGLWidget.__init__(self, parent)
         self.setWindowTitle("Demo")
         self.setFixedSize(500,500)
 
-#        self._galaxy = galaxy
+        self._galaxy = galaxy_obj
+        self._scale = float(scale)
+        self._timeout = timeout
 
+        self._timer = c.QTimer(self)
+        self._timer.timeout.connect(self.run)
+        
+    def run(self):
+        self._galaxy.run()
+        self.updateGL()
+
+    def start(self):
+        self._timer.start(self._timeout)
+        
     def paintGL(self):
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
         gl.glColor3f(0.6,0.8,1.0)
+        N = self._galaxy.getCelaNum()
         gl.glBegin(gl.GL_POINTS)
-        gl.glVertex2f(0,0)
-        gl.glVertex2f(0,0.1)
-        gl.glVertex2f(0,0.2)
+        for i in xrange(N):
+            array = galaxy.celaArray_frompointer(self._galaxy.output())
+            x = array[i].p.x / self._scale
+            y = array[i].p.y / self._scale
+            # z = array[i].p.z / self._scale
+            gl.glVertex2f(x, y)
         gl.glEnd()
         
     def initializeGL(self):
@@ -32,8 +54,3 @@ class DisplayWidget(qgl.QGLWidget):
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glLoadIdentity()
         
-        
-# from PyQt4.QtGui import QApplication
-# app = QApplication([])
-d = DisplayWidget()
-d.show()
