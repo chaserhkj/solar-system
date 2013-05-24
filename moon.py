4793250dcd46f7d5cc2c7b2e56ba78dc088b2719
Light <sgsdxzy@gmail.com>
2013-05-24 20:16:45 +0800

    Updated moon demo

--- moon.py
+++ moon.py
@@ -4,19 +4,21 @@ import math
 from PyQt4.QtGui import QApplication
 
 array = galaxy.celaArray(3)
+graphic=[{"radius":14,"color":[1.0,0,0]}]
 sun = galaxy.cela(0, galaxy.vector(0,0,0), galaxy.vector(0,0,0),1000000,1,"Sun")
 array[0] = sun
-array[1]=galaxy.cela(1,
-        galaxy.vector(400,0,0),galaxy.vector(0,math.sqrt(250),0),0.1,0,"Earth") 
+array[1]=galaxy.cela(1, galaxy.vector(400,0,0),
+        galaxy.vector(0,math.sqrt(250),0),1,1,"Earth")
+graphic.append({"radius":4,"color":[0,0,1.0]})
 array[2]=galaxy.cela(2, galaxy.vector(401,0,0),
-        galaxy.vector(0,math.sqrt(250)+math.sqrt(0.01),0),0.001,0,"Moon")
-size=[30,7,5]
-g = galaxy.galaxy(3, array, step=0.01, G=0.1, t=0, r=6, o=0.1, aplfx=False)
+        galaxy.vector(0,math.sqrt(250),0),1,1,"Moon")
+graphic.append({"radius":1,"color":[0,1.0,0]})
+g = galaxy.galaxy(3, array, step=0.01, G=0.1, t=0, r=69, o=0.01, aplfx=False)
 
 app = QApplication([])
-d = Display.DisplayWidget(g,size ,scale=400,plane_scale =
-        600,cell_density=20)
+d = Display.DisplayWidget(g,graphic ,scale=400,plane_scale =
+        600,cell_density=20,interval=20)
 d.show()
 d.start()
-#d.toggleFullScreen()
 app.exec_()
+#d.toggleFullScreen()
